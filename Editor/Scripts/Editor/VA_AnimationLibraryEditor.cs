a192fed5e9141b97fb9fd847eef3884561e47786
max <max+github@vertexcolor.xyz>
2021-01-19 13:59:54 +0100

    Preview loaded animations
    Preview to be loaded animations in the library.

---
+++ Editor/Scripts/Editor/VA_AnimationLibraryEditor.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace TAO.VertexAnimation.Editor
+{
+	[CustomEditor(typeof(VA_AnimationLibrary))]
+	public class VA_AnimationLibraryEditor : UnityEditor.Editor
+	{
+		private VA_AnimationLibrary library = null;
+
+		void OnEnable()
+		{
+			library = target as VA_AnimationLibrary;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			InputGUI();
+			EditorGUILayoutUtils.HorizontalLine(color: Color.gray);
+			InfoGUI();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private void InputGUI()
+		{
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("animationBooks"));
+		}
+
+		private void InfoGUI()
+		{
+			using (new EditorGUI.DisabledScope(disabled: true))
+			{
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("loadedAnimationsPreview"));
+			}
+		}
+	}
+}
